428b3075238142f0d4ec8e4e5f78404c09cf90c6
Boss Kerobale <bosskerobale15@gmail.com>
2026-09-13 20:08:44 +0200

    Question 1: Countdown

---
+++ question1.java
@@ -0,0 +1,18 @@
+/*
+* Student Name: Bosa Jeremiah
+* Student ID: 24019732
+* Question 1
+*/
+public class question1 {
+    public static void main (String[] args) {
+        // Initialise the countdown
+        int count = 10;
+
+        while (count > 0) {
+            System.out.println(count);
+            count--;
+        }
+        System.out.println("Liftoff!!!");
+
+    }
+}
