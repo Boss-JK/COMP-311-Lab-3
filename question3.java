904089dea08fc8c6b59eebea43c0cf67a8f719f6
Boss Kerobale <bosskerobale15@gmail.com>
2026-09-13 20:31:20 +0200

    Question 3:  Even Numbers between 1 and 50

---
+++ question3.java
@@ -0,0 +1,19 @@
+/*
+* Student Name: Bosa Jeremiah
+* Student ID: 24019732
+* Question 3
+*/
+public class question3 {
+    public static void main (String[] args) {
+        // Intializing the even number count
+        int evenNumber = 1;
+        // Getting the even numbers between 1 and 50
+        while (evenNumber <= 50){
+            if (evenNumber % 2 == 0){
+                System.out.println(evenNumber);
+            }
+            evenNumber++;
+        }
+    }
+    
+}
