00546a81b9618896cd733000240c4d137a2915fa
Boss Kerobale <bosskerobale15@gmail.com>
2026-09-13 20:47:34 +0200

    Question 4: Repeating menu

---
+++ question4.java
@@ -0,0 +1,26 @@
+/*
+* Student Name: Bosa Jeremiah Kerobale
+* Student ID: 24019732
+* Question 4
+*/
+
+// Importing a scanner
+import java.util.Scanner;
+public class question4 {
+    public static void main (String[] args) {
+    // Creating a scanner
+    Scanner console = new Scanner (System.in);
+
+    // Ask the user to enter 1 or 2
+    System.out.print("Enter one or two: ");
+    // Reading the user's input
+    int number = console.nextInt();
+    console.close();
+
+    do{
+        System.out.println("1. Continue");
+        System.out.println("2. Exit");
+    } while (number != 2);
+
+    }
+}
